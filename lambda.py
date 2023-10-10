bae88c625d0f3c692685e98a5d47c22d664b445c
XenonGoblin <20507389+XenonGoblin@users.noreply.github.com>
2023-10-09 22:49:14 -0400

    feat: lambda update

--- lambda.py
+++ lambda.py
@@ -72,35 +72,32 @@ def lambda_handler(event, context):
 
 
 import json
-import boto3
+import sagemaker
 import base64
+from sagemaker.serializers import IdentitySerializer
 
-s3 = boto3.client('s3')
+# Fill this in with the name of your deployed model
+ENDPOINT = "image-classification-endpoint"
 
 def lambda_handler(event, context):
-    """A function to serialize target data from S3"""
-    
-    # Get the s3 address from the Step Function event input
-    key = event["s3_key"]
-    bucket = event["s3_bucket"]
-    
-    # Download the data from s3 to /tmp/image.png
-    s3.download_file(bucket, key, "/tmp/image.png")
-    
-    # We read the data from a file
-    with open("/tmp/image.png", "rb") as f:
-        image_data = base64.b64encode(f.read())
 
-    # Pass the data back to the Step Function
-    print("Event:", event.keys())
+    # Decode the image data
+    image = base64.b64decode(event["body"]["image_data"])
+
+    # Instantiate a Predictor
+    predictor = sagemaker.predictor.RealTimePredictor(ENDPOINT)
+
+    # For this model the IdentitySerializer needs to be "image/png"
+    predictor.serializer = IdentitySerializer("image/png")
+
+    # Make a prediction:
+    inferences = predictor.predict(image)
+
+    # We return the data back to the Step Function    
+    event["inferences"] = inferences.decode('utf-8')
     return {
         'statusCode': 200,
-        'body': {
-            "image_data": image_data,
-            "s3_bucket": bucket,
-            "s3_key": key,
-            "inferences": []
-        }
+        'body': json.dumps(event)
     }
 
 
@@ -111,12 +108,16 @@ def lambda_handler(event, context):
 
 import json
 
-THRESHOLD = .70
+THRESHOLD = .93
+
 
 def lambda_handler(event, context):
-    
+
     # Grab the inferences from the event
-    inferences = event["inferences"]
+    body = json.loads(event['body'])
+    inferences = json.loads(body['inferences'])
+    print(inferences)
+    inferences = event["body"]["inferences"]
     
     # Check if any values in our inferences are above THRESHOLD
     meets_threshold = any([float(x.split(":")[1]) >= THRESHOLD for x in inferences.split(",")])
